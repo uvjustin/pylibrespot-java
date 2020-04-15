eea66f64edf7c25b14284b24c7fc4b233d6c09e0
Justin Wong <46082645+uvjustin@users.noreply.github.com>
2020-04-15 17:40:52 +0800

    Remove recursion in handler and changes for pylint

--- pylibrespot_java/__init__.py
+++ pylibrespot_java/__init__.py
@@ -3,9 +3,19 @@ __version__ = "0.1.0"
 import asyncio
 import logging
 import aiohttp
-from typing import Tuple
 
 _LOGGER = logging.getLogger(__name__)
+
+
+def _debug_string(string_base, status) -> str:
+    """Helper for logger debug strings."""
+    if status == 204:
+        return string_base + " No active session"
+    if status == 500:
+        return string_base + " Invalid session"
+    if status == 503:
+        return string_base + " Session is reconnecting"
+    return ""
 
 
 class LibrespotJavaAPI:
@@ -29,11 +39,9 @@ class LibrespotJavaAPI:
         """Websocket handler daemon."""
         _LOGGER.debug("Starting websocket handler")
         url = f"http://{self._ip_address}:{self._api_port}/events"
-        try:
-            while True:
-                async with self._websession.ws_connect(
-                    url
-                ) as ws:
+        while True:
+            try:
+                async with self._websession.ws_connect(url) as ws:
                     async for msg in ws:
                         json = msg.json()
                         _LOGGER.debug("Message received: %s", json)
@@ -43,113 +51,120 @@ class LibrespotJavaAPI:
                     websocket_reconnect_time,
                 )
                 await asyncio.sleep(websocket_reconnect_time)
-        except (asyncio.TimeoutError, aiohttp.ClientError):
-            _LOGGER.error(
-                "Can not connect to WebSocket at %s, will retry in %s seconds.",
-                url,
-                websocket_reconnect_time,
-            )
-            await asyncio.sleep(websocket_reconnect_time)
-            await self.start_websocket_handler(
-                ws_port, update_callback, websocket_reconnect_time
-            )
-
-    def _debug_string(self,string_base,status) -> str:
-        """Helper for logger debug strings."""
-        if status==204:
-            return string_base+" No active session"
-        if status==500:
-            return string_base + " Invalid session"
-        if status==503:
-            return string_base+" Session is reconnecting"
+            except (asyncio.TimeoutError, aiohttp.ClientError):
+                _LOGGER.error(
+                    "Can not connect to WebSocket at %s, will retry in %s seconds.",
+                    url,
+                    websocket_reconnect_time,
+                )
+                await asyncio.sleep(websocket_reconnect_time)
+                continue
 
     async def player_load(self, uri, play) -> int:
         """Load track from URI."""
-        resp = await self.post_request(endpoint="player/load",data={'uri':uri,'play':play})
-        if resp.status in [204,500,503]:
-            _LOGGER.debug(self._debug_string("Unable to load track.",resp.status))
+        resp = await self.post_request(
+            endpoint="player/load", data={"uri": uri, "play": play}
+        )
+        if resp.status in [204, 500, 503]:
+            _LOGGER.debug(_debug_string("Unable to load track.", resp.status))
         return resp.status
 
     async def player_pause(self) -> int:
         """Pause playback."""
         resp = await self.post_request(endpoint="player/pause")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string("Unable to pause player.", resp.status))
+            _LOGGER.debug(_debug_string("Unable to pause player.", resp.status))
         return resp.status
 
     async def player_resume(self) -> int:
         """Resume playback."""
         resp = await self.post_request(endpoint="player/resume")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string("Unable to resume player.", resp.status))
+            _LOGGER.debug(_debug_string("Unable to resume player.", resp.status))
         return resp.status
 
     async def player_next(self) -> int:
         """Skip to next track."""
         resp = await self.post_request(endpoint="player/next")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string("Unable to skip to the next track.", resp.status))
+            _LOGGER.debug(
+                _debug_string("Unable to skip to the next track.", resp.status)
+            )
         return resp.status
 
     async def player_prev(self) -> int:
         """Skip to previous track."""
         resp = await self.post_request(endpoint="player/prev")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string("Unable to skip to the previous track.", resp.status))
+            _LOGGER.debug(
+                _debug_string("Unable to skip to the previous track.", resp.status)
+            )
         return resp.status
 
-    async def player_set_volume(self,volume: int) -> int:
+    async def player_set_volume(self, volume: int) -> int:
         """Set volume to a given volume between 0 and 65536."""
-        resp = await self.post_request(endpoint="player/set-volume",data={'volume':volume})
+        resp = await self.post_request(
+            endpoint="player/set-volume", data={"volume": volume}
+        )
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string("Unable to set the volume.", resp.status))
+            _LOGGER.debug(_debug_string("Unable to set the volume.", resp.status))
         return resp.status
-    
+
     async def player_volume_up(self) -> int:
         """Turn up the volume a little bit."""
         resp = await self.post_request(endpoint="player/volume-up")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string("Unable to turn the volume up.", resp.status))
+            _LOGGER.debug(_debug_string("Unable to turn the volume up.", resp.status))
         return resp.status
-    
+
     async def player_volume_down(self) -> int:
         """Turn down the volume a little bit."""
         resp = await self.post_request(endpoint="player/volume-down")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string("Unable to turn the volume down.", resp.status))
+            _LOGGER.debug(_debug_string("Unable to turn the volume down.", resp.status))
         return resp.status
-    
+
     async def player_current(self) -> dict:
         """Retrieve information about the current track."""
         resp = await self.post_request(endpoint="player/current")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string("Unable to retrieve information about the current track.", resp.status))
+            _LOGGER.debug(
+                _debug_string(
+                    "Unable to retrieve information about the current track.",
+                    resp.status,
+                )
+            )
         json = await resp.json(content_type=None)
         return json
 
-    async def metadata(self,uri: str) -> dict:
+    async def metadata(self, uri: str) -> dict:
         """Retrieve metadata."""
         resp = await self.post_request(endpoint=f"metadata/{uri}")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string(f"Unable to get metadata for {uri}.", resp.status))
+            _LOGGER.debug(
+                _debug_string(f"Unable to get metadata for {uri}.", resp.status)
+            )
         json = await resp.json(content_type=None)
         return json
 
-    async def search(self,query: str) -> dict:
+    async def search(self, query: str) -> dict:
         """Make a search."""
         resp = await self.post_request(endpoint=f"search/{query}")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string(f"Unable to search for {query}.", resp.status))
+            _LOGGER.debug(_debug_string(f"Unable to search for {query}.", resp.status))
         json = await resp.json(content_type=None)
         return json
 
-    async def token(self,scope: str) -> dict:
+    async def token(self, scope: str) -> dict:
         """Request an access token for a specific scope."""
         resp = await self.post_request(endpoint=f"token/{scope}")
         if resp.status in [204, 500, 503]:
-            _LOGGER.debug(self._debug_string(f"Unable to get token for {scope}.", resp.status))
+            _LOGGER.debug(
+                _debug_string(f"Unable to get token for {scope}.", resp.status)
+            )
         json = await resp.json(content_type=None)
         return json
+
 
 class LibrespotJavaData:
     """Represent a librespot-java server."""
@@ -162,34 +177,35 @@ class LibrespotJavaData:
 
     @property
     def name(self):
+        """Name getter."""
         return "librespot-java"
 
     @property
     def player_status(self):
+        """Player status getter."""
         return self._player_status
 
     @player_status.setter
     def player_status(self, value):
+        """Player status setter."""
         self._player_status = value
 
     @property
     def volume(self):
+        """Volume getter."""
         return self._volume
 
     @volume.setter
     def volume(self, value):
+        """Volume setter."""
         self._volume = value
 
     @property
     def track_info(self):
+        """Track info getter."""
         return self._track_info
 
     @track_info.setter
     def track_info(self, value):
+        """Track info setter."""
         self._track_info = value
-
-
-async def main():
-    with aiohttp.ClientSession() as session:
-        api=LibrespotJavaAPI(session,'10.0.0.134',24879)
-        await api.player_resume()
